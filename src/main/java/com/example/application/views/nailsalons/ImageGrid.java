231031ac6fa277eada6f658e746b0a5f4c9cfb03
ahitner <aviad.hitner@intel.com>
2021-08-26 21:32:32 +0300

    when clicking a company opens company page

--- src/main/java/com/example/application/views/nailsalons/ImageGrid.java
+++ src/main/java/com/example/application/views/nailsalons/ImageGrid.java
@@ -1,7 +1,9 @@
 package com.example.application.views.nailsalons;
 
 import com.example.application.data.entity.Company;
+import com.example.application.views.cares.CompanyView;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.RouteParameters;
 
 import java.util.List;
 
@@ -10,6 +12,7 @@ public class ImageGrid extends VerticalLayout {
     public ImageGrid(List<Company> companyList){
         for(Company company : companyList){
         ImageCard item = new ImageCard(company.getName(), company.getMainImageURL());
+        this.addClickListener(e -> openCompany(company.getId()));
         add(item);
        }
     }
@@ -17,7 +20,13 @@ public class ImageGrid extends VerticalLayout {
         removeAll();
         for(Company company : companyList){
             ImageCard item = new ImageCard(company.getName(), company.getMainImageURL());
+            item.getElement().addEventListener("open-company", e -> openCompany(company.getId()));
             add(item);
         }
+    }
+    private void openCompany(Integer id) {
+        getUI().ifPresent(ui -> ui.navigate(
+                        CompanyView.class,
+                        new RouteParameters("companyID", String.valueOf(id))));
     }
 }
